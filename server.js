d57aebba3f8f235a22b786c2ca7d5411239b0b09
BernabeJudicael <bernabejudicael02@gmail.com>
2026-01-02 09:25:32 +0100

    ma modif server

--- server.js
+++ server.js
@@ -33,12 +33,13 @@ app.use(cors({
         'http://localhost:3000',
         'http://localhost:3001',
         'http://localhost:5173',
-        'https://portfolio-judicael.vercel.app'
+        process.env.FRONTEND_URL
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
